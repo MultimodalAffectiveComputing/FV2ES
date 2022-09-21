fc4b06d03a04cde57d30fc64b5fdc9dd6091a23f
Multimodal Affective Computing <88121425+qlwei89@users.noreply.github.com>
2022-09-21 15:12:25 +0800

    Add files via upload

---
+++ FV2ES/V2EM_prediction/src/e2e_t.py
@@ -0,0 +1,16 @@
+from torch import nn
+from transformers import AlbertModel
+
+class MME2E_T(nn.Module):
+    def __init__(self, feature_dim, num_classes=4, size='base'):
+        super(MME2E_T, self).__init__()
+        self.albert = AlbertModel.from_pretrained('./src/models/albert-base-v2')
+
+    def forward(self, text, get_cls=False):
+        last_hidden_state = self.albert(**text).last_hidden_state
+        if get_cls:
+            cls_feature = last_hidden_state[:,0]
+            return cls_feature
+
+        text_features = self.text_feature_affine(last_hidden_state).sum(1)
+        return text_features
